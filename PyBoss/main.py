b641e60707c1db4fa0e7a17a6e5356e5cb56bbc8
Joshua Muchmore <jsmuchmore@gmail.com>
2018-05-03 11:36:33 -0500

    pyboss finished

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -2,6 +2,7 @@ import csv
 import os
 
 csvpath = os.path.join('resources', 'employee_data1.csv')
+output_path = "employee_data_cleaned.csv"
 
 states = {
     'Alabama': 'AL',
@@ -56,24 +57,52 @@ states = {
     'Wyoming': 'WY',
 }
 
-
+states_abr = []
+names = []
+employee_id = []
+bday = []
+bday_sorted = []
+ssn = []
+first_name = []
+last_name = []
 
 with open(csvpath, newline='') as csvfile:
     csvread = csv.reader(csvfile, delimiter=',')
 
     for row in csvread:
-        names =  row[1].split(" ")
+        names.append(row[1].split(" "))
+
+        employee_id.append(row[0])
+
+        bday.append(row[2].split("-"))
+        
+        ssn.append(row[3])
 
         if row[4] != 'State':
-            states_abr = states[row[4]]
+            states_abr.append(states[row[4]])
 
-    
+del names[0]
+del employee_id[0]
+del bday[0]
+del ssn[0]
+
+for row in names:
+    first_name.append(row[0])
+    last_name.append(row[1])
+
+for row in bday:
+    bday_sorted.append(row[1]+ "/" + row[2] + "/" + row[0])
+
+for row in ssn:
+    row = row.replace(row[:6], "***-**-")
+
+final = zip(employee_id, first_name, last_name, bday_sorted, ssn, states_abr)
 
 
+with open(output_path, 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    csvwriter.writerow(["ID", "First Name", "Last Name", "DOB", "SSN", "State"])
+    csvwriter.writerows(final)
 
-for name in names:
-    print(name)
 
-for state in states_abr:
-    print(state)
 
